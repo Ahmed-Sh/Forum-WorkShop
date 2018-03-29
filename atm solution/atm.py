8bd72fddc4d6281a057c1cac9e04a30cd4b5b1a9
Ahmed Shalaby <ahshalaby76@yahoo.com>
2018-03-30 00:16:55 +0200

    Adding show_withdrawals function

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -2,11 +2,12 @@ class Atm():
     def __init__(self ,balance ,bank_name):
         self.balance = balance
         self.bank_name = bank_name
-        
+        self.withdrawals_list = []
+
     def withdraw(self,request):
         print "Welcome to {0} ".format(self.bank_name)
         print "Current balance = {0} ".format(self.balance)
-        print "=================================="
+        print "=" * 30
 
         if request > self.balance:
             print "You don't have that much !!"
@@ -15,6 +16,7 @@ class Atm():
             print "Error , Wrong request !!!!!"
 
         else:
+            self.withdrawals_list.append(request)
             self.balance -= request
 
             while request >= 100:
@@ -38,9 +40,17 @@ class Atm():
                 request -= request
                 break
 
-        print "=================================="
+        print "=" * 30
 
         return self.balance
+
+    def show_withdrawals(self):
+        for withdrawal in self.withdrawals_list:
+            print "Withdrawal No. %d = %d" % ((self.withdrawals_list.index(withdrawal)+1),withdrawal)
+
+        print "Total withdrawal =",sum(self.withdrawals_list)
+        print "Your balance now =",self.balance
+        print "=" * 30
 
 balance1 = 500
 balance2 = 1000
@@ -50,7 +60,9 @@ atm2 = Atm(balance2 , "Baraka Bank")
 
 
 atm1.withdraw(277)
-atm1.withdraw(800)
+atm1.withdraw(217)
+atm1.show_withdrawals()
 
 atm2.withdraw(100)
 atm2.withdraw(2000)
+atm2.show_withdrawals()
