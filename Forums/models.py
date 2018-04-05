c3a660231f8b8e7ed645b7a879edb504d359c733
Ahmed Shalaby <ahshalaby76@yahoo.com>
2018-04-05 18:34:35 +0200

    Adding get_by_id

--- Forums/models.py
+++ Forums/models.py
@@ -2,6 +2,7 @@ class Members():
     def __init__(self, name = None, age = None):
         self.name = name
         self.age = age
+        self.id = 0
 
     def set_member_data(self, name = None, age = None):
         a = "Please write your Name: "
@@ -11,12 +12,13 @@ class Members():
         print ("Name: {0} \nAge: {1}".format(self.name, self.age))
 
     def __str__(self):
-        return ("Member name: {0} \nMember age: {1}".format(self.name,self.age))
+        return ("Member name: {0} \nMember age: {1} \nMember Id: {2}".format(self.name,self.age,self.id))
 
 class Posts():
     def __init__(self, title = None, content = None):
         self.title = title
         self.content = content
+        self.id = 0
 
     def set_contents(self, title = None, content = None):
         a = "Please write your post title: "
--- Forums/store.py
+++ Forums/store.py
@@ -1,23 +1,48 @@
 class MemberStore:
     members = []
+    last_id = 1
 
     def add(self, member):
-        self.members.append(member)
+        member_list = MemberStore.members
+        if member in member_list:
+            print("Please Choose another Name")
+        else:
+            MemberStore.members.append(member)
+            member.id = MemberStore.last_id
+            MemberStore.last_id += 1
+
 
     def get_all(self):
         return MemberStore.members
 
 
+    def get_by_id(self,id):
+        all_members = self.get_all()
+        result = None
+        for member in all_members:
+            if member.id == id:
+                result = member
+                break
+        return result
+
+
 class PostsStore:
     posts = []
+    last_id = 1
 
     def add(self, post):
-        self.posts.append(post)
+        PostsStore.posts.append(post)
+        post.id = PostsStore.last_id
+        PostsStore.last_id += 1
 
     def get_all(self):
         return PostsStore.posts
 
-
-
-
-
+    def get_by_id(self,id):
+        all_posts = self.get_all()
+        result = None
+        for post in all_posts:
+            if post.id == id:
+                result = post
+                break
+        return result
