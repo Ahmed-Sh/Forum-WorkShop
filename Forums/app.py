a1a86122898889f3789a99edcbe3e077e16fb435
Ahmed Shalaby <ahshalaby76@yahoo.com>
2018-12-06 20:07:37 +0200

    Updating app.py & index.html

--- Forums/app.py
+++ Forums/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from dummy_data import post_store
 
 app = Flask(__name__)
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html")
+    posts = post_store.get_all()
+    return render_template("index.html", posts=posts)
 
 
 app.run()
